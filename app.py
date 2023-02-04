55a3010a4f3284a3789c1c0d79583bc95f02c9bb
fenceFoil <karnavasbj@gmail.com>
2023-02-04 15:01:48 -0500

    Server that broadcasts random board states

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import asyncio
 from dataclasses import dataclass
+from datetime import datetime, timedelta, timezone
 import random
 
 from fastapi import FastAPI, WebSocket
@@ -26,40 +27,51 @@ class Board:
                 "roundNumber": self.roundNumber,
                 "width": self.boardWidth,
                 "height": self.boardHeight,
-                "cells": self.boardData
+                "cells": self.boardPlayerIds
             }
         }
 
     def nextFrame(self):
         # Just shuffle them all lol
         self.roundNumber += 1
-        self.boardPlayerIds = [[random.choice("0", "1", "2", None) for x in range(self.boardHeight)] for y in range(self.boardHeight)]
+        self.boardPlayerIds = [[random.choice(["0", "1", "2", None]) for x in range(self.boardHeight)] for y in range(self.boardHeight)]
     
 sessions: list[WebSocket] = []
-board: Board
+board: Board = Board()
 shutdownSignal: bool = False
 timePerFrameMs = 2200
 
 @app.on_event("startup")
 async def setup():
-    board = Board()
     asyncio.create_task(runGameLoop())
 
 def broadcastMessage(type:str, messageData):
-    
+    messageData['type'] = type
+    for ws in sessions:
+        async def doSendOneWSMsg():
+            await ws.send_json(messageData)
+        asyncio.create_task(doSendOneWSMsg())
+
+def getBoardUpdateData():
+    boardData = board.getBoardSendData()
+    boardData['board']['nextRoundTime'] = (datetime.utcnow() + timedelta(milliseconds=timePerFrameMs)).replace(tzinfo=timezone.utc).isoformat()
+    return boardData
 
 async def runGameLoop():
     while not shutdownSignal:
         board.nextFrame()
-        broadcastMessage("board", board.getBoardSendData())
+        broadcastMessage("board", getBoardUpdateData())
         await asyncio.sleep(timePerFrameMs/1000.0)
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     sessions.append(websocket)
+    print("Session connected")
     try:
-        websocket.send_json(board.getBoardSendData())
+        msg = getBoardUpdateData()
+        msg['type'] = "board"
+        await websocket.send_json(msg)
         while True:
             data = await websocket.receive_text()
             await websocket.send_text(f"Message text was: {data}")
